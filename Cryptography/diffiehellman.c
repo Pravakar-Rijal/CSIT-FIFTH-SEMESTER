5472f96f49bf61e1fa7dcab6fe2f42d9a4c83d95
Pravakar-Rijal <86073833+Pravakar-Rijal@users.noreply.github.com>
2024-01-08 12:13:41 +0545

    Update diffiehellman.c

--- Cryptography/diffiehellman.c
+++ Cryptography/diffiehellman.c
@@ -1,72 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define q 19
 #define alpha 13
 
-int yA,yB;
+int yA, yB;
 
-long long power(long long base,long long exp,long long mod)
-{
-    long long result= 1;
+long long power(long long base, long long exp, long long mod) {
+    long long result = 1;
     base = base % mod;
-    while(exp > 0)
-    {
-        if(exp%2==1)
-        {
+    while (exp > 0) {
+        if (exp % 2 == 1) {
             result = (result * base) % mod;
         }
         exp /= 2;
-        base= (base * base)% mod;
+        base = (base * base) % mod;
     }
     return result;
 }
 
-int userA_key_generation()
-{
+int userA_key_generation() {
     int xA;
-    //Private Key for User A
-    xA=(rand()%(q-2))+2;
-    //Public Key for User A
-    yA=power(alpha,xA,q);
+    // Private Key for User A
+    xA = (rand() % (q - 2)) + 2;
+    // Public Key for User A
+    yA = power(alpha, xA, q);
     return xA;
 }
 
-int userB_key_generation()
-{
-    int xB;
-    //Private Key for User B
-    xB=(rand()%(q-2))+2;
-    //Public Key for User B
-    yB=power(alpha,xB,q);
-    return xB;
-}
-
-int userA_shared_key(int xA)
-{
-    //Shared Key by User A
+int userA_shared_key(int xA, int yB) {
+    // Shared Key by User A
     int kA;
-    kA=power(yB,xA,q);
+    kA = power(yB, xA, q);
     return kA;
 }
 
-int userB_shared_key(int xB)
-{
-    //Shared Key by User B
+int userB_key_generation() {
+    int xB;
+    // Private Key for User B
+    xB = (rand() % (q - 2)) + 2;
+    // Public Key for User B
+    yB = power(alpha, xB, q);
+    return xB;
+}
+
+int userB_shared_key(int xB, int yA) {
+    // Shared Key by User B
     int kB;
-    kB=power(yA,xB,q);
+    kB = power(yA, xB, q);
     return kB;
 }
 
-    
-int main()
-{
-    int kA,kB;
-    srand(59);
-    userA_key_generation();
-    userB_key_generation();
-    kA=userA_shared_key();
-    kB=userB_shared_key();
-    printf("\nSECRET KEY BY USER A: %d",kA);
-    printf("\nSECRET KEY BY USER B: %d",kB);
+int main() {
+    int kA, kB;
+    srand(23);
+
+    int xA = userA_key_generation();
+    int xB = userB_key_generation();
+
+    kA = userA_shared_key(xA, yB);
+    kB = userB_shared_key(xB, yA);
+
+    printf("\nSECRET KEY BY USER A: %d", kA);
+    printf("\nSECRET KEY BY USER B: %d", kB);
     return 0;
 }
